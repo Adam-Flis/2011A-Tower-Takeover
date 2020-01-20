c9c43f757db879af5f2ddf38ae33ad28da28298c
Adam F <59700364+Jelly2011@users.noreply.github.com>
2020-01-19 21:02:25 -0500

    Changes

--- src/auton_programs/Skills.cpp
+++ src/auton_programs/Skills.cpp
@@ -6,4 +6,17 @@
 //Points scored: 24
 void Skills(){
 
+  //Unfolds tray and sets state
+  Chassis->setState({0_in, 0_in, 0_deg});
+  Unfold();
+  Intake.moveVelocity(200);
+  Chassis->getModel()->setBrakeMode(AbstractMotor::brakeMode::brake);
+  Arm.setBrakeMode(AbstractMotor::brakeMode::hold);
+
+  //Intakes line of cubes
+  Chassis->setMaxVelocity(75);
+  Chassis->driveToPoint({8_ft, 0_ft});
+  Chassis->turnAngle(45_deg);
+  Chassis->
+
 }
